6659b506e7a366beb8186c55d251169ac71d736b
yaochengjian <915248459@qq.com>
2020-06-27 12:11:18 +0800

    deploy

---
+++ precache-manifest.422efdc7ef71ab15bce38aa0e117a012.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9092979961bb55a79078caeb80eeb324",
+    "url": "/morney-1/index.html"
+  },
+  {
+    "revision": "261343dcbf4fd8fb0cda",
+    "url": "/morney-1/js/app.0ebd85de.js"
+  },
+  {
+    "revision": "331f57377bfbc058c584",
+    "url": "/morney-1/js/chunk-vendors.c9c472a7.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-1/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-1/robots.txt"
+  }
+]);
